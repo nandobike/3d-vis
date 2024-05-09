0415ab26ac6f7370f0fbab74194e7a2054b0dbfd
nandobike <60858431+nandobike@users.noreply.github.com>
2024-05-08 23:37:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 import pandas as pd
 from scipy.optimize import nnls
-from os import path
+#from os import path
 
 #some old versions of scipy do not have the cumulative trapezoid function
 try:
