d00f27c71c9dc3d0365dfb1c7159a5e8dc7dc16f
nandobike <60858431+nandobike@users.noreply.github.com>
2024-05-09 01:10:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -443,8 +443,8 @@ cum_area *= total_area
 pore_range_area = np.array([2.05, 3.84]) #Enter pore range here
 
 pore_range_area_tuple = st.slider(
-    "Select a range of values",
-    0.0, 5.5, (2.05, 3.84))
+    "Select a pore range to calculate specific surface area:",
+    np.min(df_PSD_pb[0]/10), np.max(df_PSD_pb[0]/10), (2.05, 3.84))
 
 pore_range_area = np.array([pore_range_area_tuple[0], pore_range_area_tuple[1]]) #Enter pore range here
 
@@ -465,4 +465,4 @@ ax.text(pore_range_area.mean(),
         verticalalignment='center')
         #transform=ax.transAxes)
 st.pyplot(fig)        
-st.text(f'Area between {pore_range_area[0]} nm and {pore_range_area[1]} nm is {area_between:.1f} m2/g')
+st.write(f'Area between {pore_range_area[0]} nm and {pore_range_area[1]} nm is {area_between:.1f} m2/g')
