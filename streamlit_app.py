f8b4455bbe94a4026d39276d8b865a763b24f050
nandobike <60858431+nandobike@users.noreply.github.com>
2024-05-08 23:26:14 -0400

    import libraries

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
+# Import all necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import pandas as pd
+from scipy.optimize import nnls
+from os import path
+
+#some old versions of scipy do not have the cumulative trapezoid function
+try:
+    from scipy.integrate import cumulative_trapezoid
+except:
+    print("Old Scipy, cumulative PSD will throw error later")
+
+
+
 st.write('Hello world!')
